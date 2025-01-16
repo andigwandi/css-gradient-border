9142c434e92d03a14b3d889c4fccc3777e344cb9
Sanjeev Kumar <andigwandi@gmail.com>
2025-01-16 14:05:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("mousemove", handleMouseMove);
+});
+
+function handleMouseMove(e) {
+  const rect = this.getBoundingClientRect();
+  const mouseX = e.clientX - rect.left - rect.width / 2;
+  const mouseY = e.clientY - rect.top - rect.height / 2;
+
+  let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+  angle = (angle + 360) % 360;
+
+  this.style.setProperty("--start", angle + 60);
+}
